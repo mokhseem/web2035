4f973b0fce93434bf4e3d6c87dff87bfef540a96
mokhseem <mokhseem@icloud.com>
2022-02-12 23:27:37 +0300

    Upload seatwork #2

---
+++ php/lesson2/task_1.php
@@ -0,0 +1,13 @@
+<?php
+  echo <<<END
+  <section style="width: 520px; background-color: #f2f2f2">
+    <p style="text-align: center">Внимание!<p>
+    <p style="text-align: justify">Мы становимся профессиональными Web-программистами. Изучив язык PHP, мы сможем создавать:</p>
+    <ul>
+      <li>гостевые книги,</li>
+      <li>форумы,</li>
+      <li>чаты</li>
+      <li>и управлять целыми портальными системами. Мы сделаем это!</li>
+  </section>
+  END;
+?>
---
+++ php/lesson2/task_2.php
@@ -0,0 +1,11 @@
+<?php
+$a = 2;
+
+if ($a == 1 || $a == 2) {
+  echo "Hello world";
+} elseif ($a == 3) {
+  echo "Hi world";
+} else {
+  echo "Goodbye world";
+}
+?>
