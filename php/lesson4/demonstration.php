a9e552fe46894f33474d333de6d9f3c00a2db189
mokhseem <mokhseem@icloud.com>
2022-02-15 03:55:46 +0300

    Upload seatwork #4

---
+++ php/lesson4/demonstration.php
@@ -0,0 +1,6 @@
+<?php
+
+$words = explode(" ", "теперь пора всем хорошим людям прийти на помощь стране");
+echo $words[1] . " "  . $words[0] . " "  . $words[8] . " "  . $words[5] . " "  . $words[6] . " "  . $words[7] . " "  . $words[2] . " "  . $words[3] . " "  . $words[4];
+
+?>
---
+++ php/lesson4/iteration.php
@@ -0,0 +1,20 @@
+<?php 
+
+$i = 0;
+
+while (++$i < 11) {
+
+ switch ($i) {
+    case 5:
+      echo "Итерация 5";
+      break;
+    case 6:
+      echo ' ';
+      break;
+    case 10:
+      echo "Итерация 10";
+      break;
+  }
+};
+
+?>
